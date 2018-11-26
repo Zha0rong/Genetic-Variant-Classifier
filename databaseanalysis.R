18ec08fe8dd76f34c2c864955b047f4d44a22905
Zha0rong Li <zhaorong@bu.edu>
2018-11-25 22:14:06 -0500

    Another Overhaul.

--- databaseanalysis.R
+++ databaseanalysis.R
@@ -3,28 +3,19 @@ arguments=commandArgs(TRUE)
 Database=toString(arguments[1])
 library('vcfR')
 library('tidyr')
-#library('BSgenome')
-library('GenomicRanges')
-library('GenomicFeatures')
 library('Biostrings')
-#Loading the UCSC human genome assembly, NOT in current use
-#UCSCgenome=BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38
-#Load the ensembl
+library('biomaRt')
 ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
-#Specify the filters: the database provides us the chromosome names and positions.
 filters = c('chromosome_name','start','end')
-#Only two attributes here, gene name and gene type.
-attributes=c('external_gene_name','gene_biotype')
-print('Finish loading GRCh 38 human genome UCSC version.')
+attributes=c('external_gene_name','gene_biotype','name_1006') #ensembl_gene_id, name_1006
 vcf=read.vcfR(Database)
 fix=data.frame(vcf@fix,stringsAsFactors = FALSE)
 fix=fix[,-c(ncol(fix),ncol(fix)-1,ncol(fix)-2)]
 info=data.frame(extract_info_tidy(vcf),stringsAsFactors = FALSE)
 largedata=data.frame(cbind(fix,info),stringsAsFactors = FALSE)
 rm(vcf,fix,info)
-write.table(largedata,'Database.tbl',quote = FALSE,sep = '\t')
+#write.table(largedata,'Database.tbl',quote = FALSE,sep = '\t')
 print('Finish loading in the vcf format database.')
-workingdataset=largedata[,c(1,2,4,5,10,11,17,18,19,20,25)]
 #Note on dataset index: 1. Chromosome number
 #                       2. Position
 #                       3. Reference base
@@ -37,33 +28,41 @@ workingdataset=largedata[,c(1,2,4,5,10,11,17,18,19,20,25)]
 #                       10. Mutation tye
 #                       11. Consequence type
 #This for-loop strips off the SOI index (journal-related) from the data
+workingdataset=largedata[,c(1,2,4,5,10,11,17,18,19,20,25)]
 workingdataset[,11] = gsub('SO:\\d{7}\\|','',workingdataset[,11])
 workingdataset[,6] = gsub('_',' ',workingdataset[,6])
 workingdataset[,7] = gsub('_',' ',workingdataset[,7])
-write.table(workingdataset,'working.tbl',quote = FALSE,sep = '\t')
+#write.table(workingdataset,'working.tbl',quote = FALSE,sep = '\t')
 print('Finish loading the features and prior')
 rm(largedata)
 
-#This part is a duplication, later i will determine which one i will keep.
-#This part uses biomaRt to find gene information for each genetic variant.
-ensembl_info=matrix(attributes,c(1,2))
-for (i in 1:nrow(workingdataset)) {
-values=c(list(workingdataset[i,1]),list(workingdataset[i,2]),list(workingdataset[i,2]))
-ensembl_info = rbind(ensembl_info,matrix(getBM(attributes = attributes,filters = filters,values = values,mart = ensembl),c(1,2)))
-print(i/nrow(workingdataset))
-}
-ensembl_info=data.frame(matrix(unlist(ensembl_info),ncol = 2))
-colnames(ensembl_info)=ensembl_info[1,]
-ensembl_info=ensembl_info[-1,]
-workingdataset=cbind(workingdataset,ensembl_info)
-rm(i,ensembl_info)
+#This chunck of codes will handle the genetic variants as 'Conflicting of interpretation: These are the variants that researchers are not sure whether they are pathogenic or not.
+#However, the number of reports of different interpretations are provided
+Conflict_testing_data_set=subset(workingdataset,grepl('Conflicting interpretations of pathogenicity',workingdataset$CLNSIG,ignore.case = TRUE))
+Training_data_set=subset(workingdataset,!grepl('Conflicting interpretations of pathogenicity',workingdataset$CLNSIG,ignore.case = TRUE))
 
-#This chunck of for loops collects the amount of types of variants in the database
-Typesofvariant=data.frame(table(unlist(workingdataset$CLNVC)),stringsAsFactors = FALSE)
+Total_ensembel_info=data.frame(getBM(attributes,filters = filters,values = list(Training_data_set[1,1],Training_data_set[1,2],Training_data_set[1,2]),mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
+for ( i in 1:nrow(workingdataset)) {
+  values= list(Training_data_set[i,1],Training_data_set[i,2],Training_data_set[i,2])
+  a=data.frame(getBM(attributes,filters = filters,values = values,mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
+  if (nrow(a)==1) {
+    Total_ensembel_info=rbind(Total_ensembel_info,a)
+  }
+  else {
+    a[1,1]=paste(toString(a[,1]))
+    a[1,2]=paste(toString(a[,2]))
+    a[1,3]=paste(toString(a[,3]))
+    Total_ensembel_info=rbind(Total_ensembel_info,a[1,])
+  }
+  print(i)
+}
+
+#This chunck of codes collects the amount of types of variants in the database
+Typesofvariant=data.frame(table(unlist(Training_data_set$CLNVC)),stringsAsFactors = FALSE)
 colnames(Typesofvariant)[1]='Types'
 
 #This chunck of for loops collects the amount of types of consequences in the database
-consequence=data.frame(data.frame(table(workingdataset[,11]),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
+consequence=data.frame(data.frame(table(Training_data_set[,11]),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
 consequenceofvariant=list()
 for (i in 1:nrow(consequence)) {
   if (grepl(',',consequence[i,1])) {
@@ -86,10 +85,8 @@ for (i in 1:nrow(consequence)) {
   }
 }
 rm(i,j,listofconsequence,consequence)
-print('Finish loading the consequences of variants.')
-
 #This chunck of for loops collects the amount of types of diseases in the database
-disease=data.frame(data.frame(table(unlist(workingdataset[,6])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
+disease=data.frame(data.frame(table(unlist(Training_data_set[,6])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
 diseaseofvariant=list()
 for (i in 1:nrow(disease)) {
   if (grepl('\\|',disease[i,1])) {
@@ -113,9 +110,8 @@ for (i in 1:nrow(disease)) {
 }
 rm(i,j,listofdisease,disease)
 print('Finish loading the types of diseases associated with genetic variants.')
-
 #This Chunk of codes formatted the number of significance.
-variant=data.frame(data.frame(table(unlist(workingdataset[,7])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
+variant=data.frame(data.frame(table(unlist(Training_data_set[,7])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
 significanceofvariant=list()
 for (i in 1:nrow(variant)) {
   if (grepl(', ',variant[i,1])) {
@@ -134,11 +130,11 @@ for (i in 1:nrow(variant)) {
         }
       }
       else{
-      if (listofvariant[j,1] %in% significanceofvariant) {
-      }
-      else {
-        significanceofvariant[toString(listofvariant[j,1])] = toString(listofvariant[j,1])
-      }}
+        if (listofvariant[j,1] %in% significanceofvariant) {
+        }
+        else {
+          significanceofvariant[toString(listofvariant[j,1])] = toString(listofvariant[j,1])
+        }}
     }
   }
   else {
@@ -154,22 +150,15 @@ for (i in 1:nrow(variant)) {
       }
     }
     else {
-    if (variant[i,1] %in% significanceofvariant) {
-    }
-    else {
-      significanceofvariant[toString(variant[i,1])] = toString(variant[i,1])
-    }}
-
+      if (variant[i,1] %in% significanceofvariant) {
+      }
+      else {
+        significanceofvariant[toString(variant[i,1])] = toString(variant[i,1])
+      }}
+    
   }
 }
 rm(i,j,listofvariant,variant,list,k,l,a)
-print('Finish loading the clinical significance of genetic variants.')
-
-#This chunck of codes will handle the genetic variants as 'Conflicting of interpretation: These are the variants that researchers are not sure whether they are pathogenic or not.
-#However, the number of reports of different interpretations are provided
-Conflict_testing_data_set=subset(workingdataset,grepl('Conflicting interpretations of pathogenicity',workingdataset$CLNSIG,ignore.case = TRUE))
-Training_data_set=subset(workingdataset,!grepl('Conflicting interpretations of pathogenicity',workingdataset$CLNSIG,ignore.case = TRUE))
-
 #This chunck of codes calculates the prior probabilities.
 Initial_Prior_Probability=data.frame(table(Training_data_set[,7]),stringsAsFactors = FALSE)
 Prior_Probability=data.frame(matrix(significanceofvariant),stringsAsFactors = FALSE)
@@ -184,7 +173,6 @@ for (i in 1:nrow(Prior_Probability)) {
 }
 Prior_Probability$Probability=Prior_Probability$Probability/nrow(Training_data_set)
 rm(i,j,Initial_Prior_Probability)
-
 #Get the first feature likelihood.
 Pathogenic=subset(Training_data_set,grepl('pathogenic|likely pathogenic',Training_data_set$CLNSIG,ignore.case = TRUE))
 Benign=subset(Training_data_set,grepl('benign|likely benign',Training_data_set$CLNSIG,ignore.case = TRUE))
@@ -206,9 +194,6 @@ for (i in 1:nrow(Feature_type_of_variant)) {
 rm(i,j,k,p,b)
 Feature_type_of_variant$Pathogenic=(Feature_type_of_variant$Pathogenic+1)/(nrow(Pathogenic)+1*nrow(Feature_type_of_variant))
 Feature_type_of_variant$Benign=(Feature_type_of_variant$Benign+1)/(nrow(Benign)+1*nrow(Feature_type_of_variant))
-print('Finish calculating likelihood of type of variant feature.')
-
-#Get the first feature likelihood.
 Feature_type_of_consequence=data.frame(matrix(unlist(consequenceofvariant)),stringsAsFactors = FALSE)
 colnames(Feature_type_of_consequence)='Consequence'
 Feature_type_of_consequence$Pathogenic=rep(0,nrow(Feature_type_of_consequence))
@@ -229,27 +214,3 @@ for (i in 1:nrow(Feature_type_of_consequence)) {
 Feature_type_of_consequence$Pathogenic=(Feature_type_of_consequence$Pathogenic+1)/(nrow(Pathogenic)+1*nrow(Feature_type_of_consequence))
 Feature_type_of_consequence$Benign=(Feature_type_of_consequence$Benign+1)/(nrow(Benign)+1*nrow(Feature_type_of_consequence))
 rm(c,i,j,k)
-print('Finish calculating likelihood of consequence feature.')
-
-###DERELICT###
-###These are some derelict codes, which were used to find the genetic information.
-#Patho=Pathogenic[,c(1,2)]
-#Benig=Benign[,c(1,2)]
-#P=matrix(attributes,c(1,2))
-#for (i in 1:nrow(Patho)) {
-  #P = rbind(P,matrix(getBM(attributes = attributes,filters = filters,values = c(list(Patho[i,1]),list(Patho[i,2]),list(Patho[i,2])),mart = ensembl),c(1,2)))
-  #print(i/nrow(Patho))
-#}
-#P=data.frame(matrix(unlist(P),ncol = 2))
-#colnames(P)=P[1,]
-#P=P[-1,]
-#B=matrix(attributes,c(1,2))
-#for (i in 1:nrow(Benig)) {
-  #B = rbind(B,matrix(getBM(attributes = attributes,filters = filters,values = c(list(Benig[i,1]),list(Benig[i,2]),list(Benig[i,2])),mart = ensembl),c(1,2)))
-  #print(i/nrow(Benig))
-#}
-#B=data.frame(matrix(unlist(B),ncol = 2))
-#colnames(B)=B[1,]
-#B=B[-1,]
-#rm(i)
-###DERELICT###
