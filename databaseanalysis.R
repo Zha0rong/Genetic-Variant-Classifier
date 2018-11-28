9ea237355bdbf373aca053c47c3168578b726ade
Li Zhaorong <lizhaorong@dhcp-wifi-8021x-155-41-88-17.bu.edu>
2018-11-27 21:50:33 -0500

    Organize the classification methods.

--- databaseanalysis.R
+++ databaseanalysis.R
@@ -1,4 +1,5 @@
 #Read in the Variant Call Form Database
+######Prep Work######
 arguments=commandArgs(TRUE)
 Database=toString(arguments[1])
 library('vcfR')
@@ -39,16 +40,13 @@ workingdataset=getting_information(largedata)
 print('Finish loading the features and prior')
 rm(largedata)
 
-#This chunck of codes will handle the genetic variants as 'Conflicting of interpretation: These are the variants that researchers are not sure whether they are pathogenic or not.
-#However, the number of reports of different interpretations are provided
+######This chunck of codes will handle the genetic variants as 'Conflicting of interpretation: These are the variants that researchers are not sure whether they are pathogenic or not. However, the number of reports of different interpretations are provided.#######
 Conflict_testing_data_set=subset(workingdataset,grepl('Conflicting interpretations of pathogenicity',workingdataset$CLNSIG,ignore.case = TRUE))
 Training_data_set=subset(workingdataset,!grepl('Conflicting interpretations of pathogenicity',workingdataset$CLNSIG,ignore.case = TRUE))
-
-#This chunck of codes collects the amount of types of variants in the database
+######This chunck of codes collects the amount of types of variants in the database######
 Typesofvariant=data.frame(table(unlist(Training_data_set$CLNVC)),stringsAsFactors = FALSE)
 colnames(Typesofvariant)[1]='Types'
-
-#This chunck of for loops collects the amount of types of consequences in the database
+######Collects types of variants, consequences and diseases######
 Types_collectors = function(Data,column,sep) {
   data=data.frame(data.frame(table(Data[,column]),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
   result=list()
@@ -74,104 +72,63 @@ Types_collectors = function(Data,column,sep) {
   }
   return(result)
 }
-consequence=data.frame(data.frame(table(Training_data_set[,11]),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
-consequenceofvariant=list()
-for (i in 1:nrow(consequence)) {
-  if (grepl(',',consequence[i,1])) {
-    listofconsequence=data.frame(strsplit(toString(consequence[i,1]),','),stringsAsFactors = FALSE)
-    colnames(listofconsequence)='consequences'
-    for (j in 1:nrow(listofconsequence)) {
-      if (listofconsequence[j,1] %in% consequenceofvariant) {
-      }
-      else {
-        consequenceofvariant[toString(listofconsequence[j,1])] = toString(listofconsequence[j,1])
-      }
-    }
-  }
-  else {
-    if (consequence[i,1] %in% consequenceofvariant) {
-    }
-    else {
-      consequenceofvariant[toString(consequence[i,1])] = toString(consequence[i,1])
-    }
-  }
-}
-rm(i,j,listofconsequence,consequence)
-#This chunck of for loops collects the amount of types of diseases in the database
-disease=data.frame(data.frame(table(unlist(Training_data_set[,6])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
-diseaseofvariant=list()
-for (i in 1:nrow(disease)) {
-  if (grepl('\\|',disease[i,1])) {
-    listofdisease=data.frame(strsplit(toString(disease[i,1]),'\\|'),stringsAsFactors = FALSE)
-    colnames(listofdisease)='diseases'
-    for (j in 1:nrow(listofdisease)) {
-      if (listofdisease[j,1] %in% diseaseofvariant) {
-      }
-      else {
-        diseaseofvariant[toString(listofdisease[j,1])] = toString(listofdisease[j,1])
-      }
-    }
-  }
-  else {
-    if (disease[i,1] %in% diseaseofvariant) {
-    }
-    else {
-      diseaseofvariant[toString(disease[i,1])] = toString(disease[i,1])
-    }
-  }
-}
-rm(i,j,listofdisease,disease)
-print('Finish loading the types of diseases associated with genetic variants.')
-#This Chunk of codes formatted the number of significance.
-variant=data.frame(data.frame(table(unlist(Training_data_set[,7])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
-significanceofvariant=list()
-for (i in 1:nrow(variant)) {
-  if (grepl(', ',variant[i,1])) {
-    listofvariant=data.frame(strsplit(toString(variant[i,1]),', '),stringsAsFactors = FALSE)
-    colnames(listofvariant)='variants'
-    for (j in 1:nrow(listofvariant)) {
-      if (grepl('/',listofvariant[j,1])) {
-        list=data.frame(strsplit(toString(listofvariant[j,1]),'/'),stringsAsFactors = FALSE)
-        colnames(list)='variants'
-        for (k in 1:nrow(list)) {
-          if (list[k,1] %in% significanceofvariant) {
+Types_collectors_multi_sep = function(Data,column) {
+  variant=data.frame(data.frame(table(unlist(Data[,column])),stringsAsFactors = FALSE)[,1],stringsAsFactors = FALSE)
+  significanceofvariant=list()
+  for (i in 1:nrow(variant)) {
+    if (grepl(',',variant[i,1])) {
+      listofvariant=data.frame(strsplit(toString(variant[i,1]),', '),stringsAsFactors = FALSE)
+      colnames(listofvariant)='variants'
+      for (j in 1:nrow(listofvariant)) {
+        if (grepl('/',listofvariant[j,1])) {
+          list=data.frame(strsplit(toString(listofvariant[j,1]),'/'),stringsAsFactors = FALSE)
+          colnames(list)='variants'
+          for (k in 1:nrow(list)) {
+            if (list[k,1] %in% significanceofvariant) {
+            }
+            else {
+              significanceofvariant[toString(list[k,1])] = toString(list[k,1])
+            }
+          }
+        }
+        else{
+          if (listofvariant[j,1] %in% significanceofvariant) {
           }
           else {
-            significanceofvariant[toString(list[k,1])] = toString(list[k,1])
-          }
-        }
-      }
-      else{
-        if (listofvariant[j,1] %in% significanceofvariant) {
-        }
-        else {
-          significanceofvariant[toString(listofvariant[j,1])] = toString(listofvariant[j,1])
-        }}
-    }
-  }
-  else {
-    if (grepl('/',variant[i,1])) {
-      a=data.frame(strsplit(toString(variant[i,1]),'/'),stringsAsFactors = FALSE)
-      colnames(a)='variants'
-      for (l in 1:nrow(a)) {
-        if (a[l,1] %in% significanceofvariant) {
-        }
-        else {
-          significanceofvariant[toString(a[l,1])] = toString(a[l,1])
-        }
+            significanceofvariant[toString(listofvariant[j,1])] = toString(listofvariant[j,1])
+          }}
       }
     }
     else {
-      if (variant[i,1] %in% significanceofvariant) {
+      if (grepl('/',variant[i,1])) {
+        a=data.frame(strsplit(toString(variant[i,1]),'/'),stringsAsFactors = FALSE)
+        colnames(a)='variants'
+        for (l in 1:nrow(a)) {
+          if (a[l,1] %in% significanceofvariant) {
+          }
+          else {
+            significanceofvariant[toString(a[l,1])] = toString(a[l,1])
+          }
+        }
       }
       else {
-        significanceofvariant[toString(variant[i,1])] = toString(variant[i,1])
-      }}
-    
+        if (variant[i,1] %in% significanceofvariant) {
+        }
+        else {
+          significanceofvariant[toString(variant[i,1])] = toString(variant[i,1])
+        }}
+      
+    }
   }
+  return(significanceofvariant)
 }
-rm(i,j,listofvariant,variant,list,k,l,a)
-#This chunck of codes calculates the prior probabilities.
+consequenceofvariant=Types_collectors(Training_data_set,11,',')
+diseaseofvariant=Types_collectors(Training_data_set,6,'\\|')
+significanceofvariant=Types_collectors_multi_sep(Training_data_set,7)
+Typesofvariant=data.frame(table(unlist(Training_data_set$CLNVC)),stringsAsFactors = FALSE)
+colnames(Typesofvariant)[1]='Types'
+
+######This chunck of codes calculates the prior probabilities.#######
 Initial_Prior_Probability=data.frame(table(Training_data_set[,7]),stringsAsFactors = FALSE)
 Prior_Probability=data.frame(matrix(significanceofvariant),stringsAsFactors = FALSE)
 Prior_Probability$Probability=rep(0,nrow(Prior_Probability))
@@ -185,10 +142,36 @@ for (i in 1:nrow(Prior_Probability)) {
 }
 Prior_Probability$Probability=Prior_Probability$Probability/nrow(Training_data_set)
 rm(i,j,Initial_Prior_Probability)
-#Get the first feature likelihood.
+
+
+
+
+
+######Get the features' likelihood.######
+feature_calculator = function (Pathogenic,Benign,column,feature,output) {
+  result=data.frame(matrix(unlist(feature)),stringsAsFactors = FALSE)
+  colnames(result)=output
+  result$Pathogenic=rep(0,nrow(result))
+  result$Benign=rep(0,nrow(result))
+  for (i in 1:nrow(result)) {
+    c=result[i,1]
+    for (j in 1:nrow(Pathogenic)) {
+      if (grepl(c,toString(Pathogenic[j,column]),ignore.case = TRUE)) {
+        result[i,2] = result[i,2]+1
+      }
+    }
+    for (k in 1:nrow(Benign)) {
+      if (grepl(c,toString(Benign[k,column]),ignore.case = TRUE)) {
+        result[i,3] = result[i,3]+1
+      }
+    }
+  }
+  result$Pathogenic=(result$Pathogenic+1)/(nrow(Pathogenic)+1*nrow(result))
+  result$Benign=(result$Benign+1)/(nrow(Benign)+1*nrow(result))
+  return(result)
+}
 Pathogenic=subset(Training_data_set,grepl('pathogenic|likely pathogenic',Training_data_set$CLNSIG,ignore.case = TRUE))
 Benign=subset(Training_data_set,grepl('benign|likely benign',Training_data_set$CLNSIG,ignore.case = TRUE))
-#The results fit with the prior probability, ready to proceed.
 Feature_type_of_variant=data.frame(Typesofvariant[,1],stringsAsFactors = FALSE)
 colnames(Feature_type_of_variant)[1]='Variant'
 Feature_type_of_variant$Pathogenic=rep(0,nrow(Feature_type_of_variant))
@@ -226,49 +209,45 @@ for (i in 1:nrow(Feature_type_of_consequence)) {
 Feature_type_of_consequence$Pathogenic=(Feature_type_of_consequence$Pathogenic+1)/(nrow(Pathogenic)+1*nrow(Feature_type_of_consequence))
 Feature_type_of_consequence$Benign=(Feature_type_of_consequence$Benign+1)/(nrow(Benign)+1*nrow(Feature_type_of_consequence))
 rm(c,i,j,k)
-
-
-####################################################
-Pathogen_ensembel_info=data.frame(getBM(attributes,filters = filters,values = list(Pathogenic[1,1],Pathogenic[1,2],Pathogenic[1,2]),mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
-Pathogen_ensembel_info[1,1]=paste(toString(Pathogen_ensembel_info[,1]))
-Pathogen_ensembel_info[1,2]=paste(toString(Pathogen_ensembel_info[,2]))
-Pathogen_ensembel_info[1,3]=paste(toString(Pathogen_ensembel_info[,3]))
-Pathogen_ensembel_info=Pathogen_ensembel_info[1,]
-for ( i in 1:nrow(Pathogenic)) {
-  values= list(Pathogenic[i,1],Pathogenic[i,2],Pathogenic[i,2])
-  a=data.frame(getBM(attributes,filters = filters,values = values,mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
-  if (nrow(a)==1) {
-    Pathogen_ensembel_info=rbind(Pathogen_ensembel_info,a)
+######Ensembl Information retrieve, do not use now.######
+ensembl_info_retrieve=function(Data,ensembl,filters,attributes){
+  Result=data.frame(getBM(attributes,filters = filters,values = list(Data[1,1],Data[1,2],Data[1,2]),mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
+  Result[1,1]=paste(toString(Result[,1]))
+  Result[1,2]=paste(toString(Result[,2]))
+  Result[1,3]=paste(toString(Result[,3]))
+  Result=Result[1,]
+  for ( i in 1:nrow(Data)) {
+    values= list(Data[i,1],Data[i,2],Data[i,2])
+    a=data.frame(getBM(attributes,filters = filters,values = values,mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
+    if (nrow(a)==1) {
+      Result=rbind(Result,a)
+    }
+    else {
+      a[1,1]=paste(toString(a[,1]))
+      a[1,2]=paste(toString(a[,2]))
+      a[1,3]=paste(toString(a[,3]))
+      Result=rbind(Result,a[1,])
+    }
+    print(i)
   }
-  else {
-    a[1,1]=paste(toString(a[,1]))
-    a[1,2]=paste(toString(a[,2]))
-    a[1,3]=paste(toString(a[,3]))
-    Pathogen_ensembel_info=rbind(Pathogen_ensembel_info,a[1,])
-  }
-  print(i)
+  rm(i,values,a)
+  Data=cbind(Data,Result)
+  return(Data)
 }
-rm(i,values,a)
-Pathogenic=cbind(Pathogenic,Pathogen_ensembel_info)
-#####################################################
-Benign_ensembel_info=data.frame(getBM(attributes,filters = filters,values = list(Benign[1,1],Benign[1,2],Benign[1,2]),mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
-Benign_ensembel_info[1,1]=paste(toString(Benign_ensembel_info[,1]))
-Benign_ensembel_info[1,2]=paste(toString(Benign_ensembel_info[,2]))
-Benign_ensembel_info[1,3]=paste(toString(Benign_ensembel_info[,3]))
-Benign_ensembel_info=Benign_ensembel_info[1,]
-for ( i in 1:nrow(Benign)) {
-  values= list(Benign[i,1],Benign[i,2],Benign[i,2])
-  a=data.frame(getBM(attributes,filters = filters,values = values,mart = ensembl,uniqueRows = TRUE),stringsAsFactors = FALSE)
-  if (nrow(a)==1) {
-    Benign_ensembel_info=rbind(Benign_ensembel_info,a)
-  }
-  else {
-    a[1,1]=paste(toString(a[,1]))
-    a[1,2]=paste(toString(a[,2]))
-    a[1,3]=paste(toString(a[,3]))
-    Benign_ensembel_info=rbind(Benign_ensembel_info,a[1,])
-  }
-  print(i)
+Pathogenic=ensembl_info_retrieve(Pathogenic,ensembl,filters,attributes)
+Benign=ensembl_info_retrieve(Benign,ensembl,filters,attributes)
+######Continue to get features' likelihood######
+Gene_type_of_Variant_Pathogenic=Types_collectors(Pathogenic,13,', ')
+Gene_type_of_Variant_Benign=Types_collectors(Benign,13,', ')
+Gene_type_of_Variant=list()
+for (i in 1:length(Gene_type_of_Variant_Benign)) {
+  a=toString(Gene_type_of_Variant_Benign[i])
+  if (a %in% Gene_type_of_Variant) {}
+  else {Gene_type_of_Variant[a]=a}
 }
-Benign=cbind(Benign,Benign_ensembel_info)
-####################################################
+for (i in 1:length(Gene_type_of_Variant_Pathogenic)) {
+  a=toString(Gene_type_of_Variant_Pathogenic[i])
+  if (a %in% Gene_type_of_Variant) {}
+  else {Gene_type_of_Variant[a]=a}
+}
+rm(Gene_type_of_Variant_Benign,Gene_type_of_Variant_Pathogenic)
